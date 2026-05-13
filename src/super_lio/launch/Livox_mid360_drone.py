696b852f9ab261e296399411bd3cd073fa561d70
ypat <ypat_999@163.com>
2026-05-13 12:15:07 +0800

    倒置雷达机型

--- src/super_lio/launch/Livox_mid360_drone.py
+++ src/super_lio/launch/Livox_mid360_drone.py
@@ -166,7 +166,8 @@ def generate_launch_description():
         executable='static_transform_publisher',
         parameters=[{'use_sim_time': DEFAULT_USE_SIM_TIME}],
         # arguments=['0.1', '0', '0.1', '0', '0.0', '0', 'base_link', 'livox_frame'],
-        arguments=['-0.1', '0', '-0.1', '0', str(deg_to_rad(-25)), '0', 'livox_frame', 'base_link'],
+        # arguments=['-0.1', '0', '-0.1', '0', str(deg_to_rad(-25)), '0', 'livox_frame', 'base_link'], # 上置雷达
+        arguments=['-0.05', '0', '-0.15', str(deg_to_rad(180)), str(deg_to_rad(210)), '0', 'livox_frame', 'base_link'],
         output='screen'
     )
     ld.add_action(livox_frame_to_base_link_tf)

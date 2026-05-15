d28ad85d529a94d6854e7e392fb58db6fdf09f46
ywj <ypat999@gmail.com>
2026-05-15 13:37:08 +0800

    调整仿真环境配置

--- src/super_lio/launch/gazebo_mid360_drone.py
+++ src/super_lio/launch/gazebo_mid360_drone.py
@@ -150,7 +150,7 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         parameters=[{'use_sim_time': DEFAULT_USE_SIM_TIME}],
-        arguments=['0.0', '0', '0.0', deg_to_rad(0), deg_to_rad(0), deg_to_rad(0), 'imu', 'x500_depth_0/livox_frame/mid360_lidar'],
+        arguments=['0.0', '0', '0.0', deg_to_rad(0), deg_to_rad(0), deg_to_rad(0), 'imu', 'x500_depth_0/livox_down_frame/mid360_down_lidar'],
         output='screen'
     )
     # ld.add_action(imu_to_livox_frame_tf)
@@ -162,7 +162,7 @@ def generate_launch_description():
         executable='static_transform_publisher',
         parameters=[{'use_sim_time': DEFAULT_USE_SIM_TIME}],
         # arguments=['0.1', '0', '0.1', '0', '0.0', '0', 'base_link', 'livox_frame'],
-        arguments=['-0.1', '0', '-0.3', deg_to_rad(0), deg_to_rad(-30), '0', 'x500_depth_0/livox_frame/mid360_lidar', 'x500_depth_0/base_link'],
+        arguments=['-0.1', '0', '-0.2', deg_to_rad(0), deg_to_rad(210), '0', 'x500_depth_0/livox_down_frame/mid360_lidar', 'x500_depth_0/base_link'],
         output='screen'
     )
     # ld.add_action(livox_frame_to_base_link_tf)
